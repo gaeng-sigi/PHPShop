dec15400725f6d18df3dac3bd69333f6daa2ff13
gaengsigi <elvin2@naver.com>
2022-07-18 17:00:53 +0900

    first commit

---
+++ application/utils/SessionUtils.php
@@ -0,0 +1,9 @@
+<?php
+    function getLoginUser() {
+        return isset($_SESSION[_LOGINUSER]) ? $_SESSION[_LOGINUSER] : null;
+    }
+
+    function getIuser() {
+        return getLoginUser() === null ? 0 : getLoginUser() -> ignore_user_abort;
+    }
+
