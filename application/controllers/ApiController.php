aee284f2452e518312095e1b6f4547498508437b
gaengsigi <elvin2@naver.com>
2022-07-19 11:42:51 +0900

    7/19

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -4,6 +4,12 @@ namespace application\controllers;
 
 class ApiController extends Controller {
     public function categoryList() {
-            return $this->model->getCategoryList();
+        return $this->model->getCategoryList();
+    }
+
+    public function productInsert() {
+        $json = getJson();
+        print_r($json);
+        return [_RESULT => $this->model->productInsert($json)];
     }
 }
